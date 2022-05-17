b82a6ad7855f5c864e8888abeb9848686d6ab666
Zachary Jones <Zachary_Jones@intuit.com>
2022-05-17 14:37:49 +0530

    App loads dark mode for the body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import { Box, Container, Link, Stack, Toolbar } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { AppBar, Box, Toolbar, Typography, Link } from '@mui/material'
 import HomeIcon from '@mui/icons-material/Home';
 import MailIcon from '@mui/icons-material/Mail';
 
@@ -20,42 +20,41 @@ const darkTheme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <AppBar position='static'>
-        <Container maxWidth='xl' sx={{ m: 0.5 }}>
-          <Toolbar disableGutters>
-            <div style={{ width: '100%' }}>
-              <Box sx={{
-                display: 'flex',
-                justifyContent: 'space-evenly',
-                p: 1,
-                m: 1,
-                bgcolor: 'background.paper',
-                borderRadius: 1,
-                alignItems: 'center'
-              }}>
-                <Link href="/" color="inherit" underline="hover">
-                  <HomeIcon />
-                </Link>
+        <Toolbar disableGutters>
+          <div style={{ width: '100%' }}>
+            <Box sx={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              p: 1,
+              m: 1,
+              //bgcolor: 'background.default',
+              borderRadius: 1,
+              alignItems: 'center'
+            }}>
+              <Link href="/" color="inherit" underline="hover">
+                <HomeIcon />
+              </Link>
 
-                <Link href="#" color="inherit" underline="hover">
-                  About
-                </Link>
+              <Link href="#" color="inherit" underline="hover">
+                About
+              </Link>
 
-                <Link href="#" color="inherit" underline="hover">
-                  Projects
-                </Link>
+              <Link href="#" color="inherit" underline="hover">
+                Projects
+              </Link>
 
-                <Link href="mailto:zachary_jones@intuit.com" color="inherit" underline="hover">
-                  <MailIcon />
-                </Link>
-              </Box>
-            </div>
-          </Toolbar>
-        </Container>
+              <Link href="mailto:zachary_jones@intuit.com" color="inherit" underline="hover">
+                <MailIcon />
+              </Link>
+            </Box>
+          </div>
+        </Toolbar>
       </AppBar>
-      <Box sx={{ p: 1, m: 1 }}>
-        <p>hello, world!</p>
-      </Box>
+      <Typography variant="h4">
+        Hello, world!
+      </Typography>
     </ThemeProvider>
   );
 }
