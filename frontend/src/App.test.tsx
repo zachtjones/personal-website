ec0444677aa3412134974ac0143376e8a49a161b
Zachary Jones <Zachary_Jones@intuit.com>
2022-05-17 12:49:37 +0530

    Added navigation header and content

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders about and projects links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('About')).toBeInTheDocument();
+  expect(screen.getByText('Projects')).toBeInTheDocument();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import { Box, Container, Link, Stack, Toolbar } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import MailIcon from '@mui/icons-material/Mail';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar position='static'>
+        <Container maxWidth='xl' sx={{ m: 0.5 }}>
+          <Toolbar disableGutters>
+            <div style={{ width: '100%' }}>
+              <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                p: 1,
+                m: 1,
+                bgcolor: 'background.paper',
+                borderRadius: 1,
+                alignItems: 'center'
+              }}>
+                <Link href="/" color="inherit" underline="hover">
+                  <HomeIcon />
+                </Link>
+
+                <Link href="#" color="inherit" underline="hover">
+                  About
+                </Link>
+
+                <Link href="#" color="inherit" underline="hover">
+                  Projects
+                </Link>
+
+                <Link href="mailto:zachary_jones@intuit.com" color="inherit" underline="hover">
+                  <MailIcon />
+                </Link>
+              </Box>
+            </div>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box sx={{ p: 1, m: 1 }}>
+        <p>hello, world!</p>
+      </Box>
+    </ThemeProvider>
   );
 }
 
