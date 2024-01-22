b036fc4752d5f55942a2081f00d88ef9764666cc
Zachary Jones <23038709+zachtjones@users.noreply.github.com>
2024-01-21 21:14:44 -0800

    Feat: Added 3-Step language classification (#2)
    Added pick a phrase for me.
    Added you guess what it is.
    
    Made the UI look basic but functional.

--- frontend/src/LanguageClassifierExperiment.tsx
+++ frontend/src/LanguageClassifierExperiment.tsx
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { LoadingButton } from "@mui/lab";
-import { Typography, Box, TextField, Grid, Divider } from "@mui/material";
+import { Typography, Box, Button, TextField, Grid, Divider, Stepper, Step, StepLabel, Stack } from "@mui/material";
 import { useState } from "react";
 import { gql } from '../src/__generated__/gql';
 import { Language } from "./__generated__/graphql";
@@ -9,23 +9,72 @@ import { BEST_MODEL_ID, GRAPHQL_URL } from './constants'
 const client = new ApolloClient({
   uri: GRAPHQL_URL,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache'
+    },
+    query: {
+      fetchPolicy: 'no-cache'
+    }
+  }
 })
 const modelId = BEST_MODEL_ID
 
 function LanguageClassifierExperiment() {
   const [phrase, setPhrase] = useState('')
-  const [result, setResult] = useState('')
-  const [probabilities, setProbabilities] = useState([] as Array<{percentageLikely: Number, language: Language}>)
   const [error, setError] = useState('')
   const [isLoading, setLoading] = useState(false)
+  const [isPicking, setPicking] = useState(false)
+  const [step, setStep] = useState(0)
+  const [yourLanguage, setYourLanguage] = useState(Language.English)
+  const [modelLanguage, setModelLanguage] = useState(Language.English)
+  const [probabilities, setProbabilities] = useState([] as Array<{percentageLikely: Number, language: Language}>)
 
-  const onSubmit = async () => {
+  const onPickClick = async () => {
+    setPicking(true)
+    setError('')
+    setPhrase('')
+
     try {
-      setLoading(true)
-      setError('')
-      setResult('')
-      setProbabilities([])
+      const query = gql(`
+        query RandomPhrase {
+          randomPhrase {
+            phrase
+            language
+          }
+        }
+      `)
 
+      const result = await client.query({
+        query: query,
+        variables: {}
+      })
+
+      if (result.errors) {
+        setError(result.errors[0].message)
+        console.log(JSON.stringify(result.errors))
+      } else {
+        setPhrase(result.data.randomPhrase.phrase)
+        // TODO - should I get the language? What would I do with it? The sample data will always have one
+      }
+    } catch (e: any) {
+      console.error(e)
+      setError(e.message)
+    }
+    setPicking(false)
+  }
+
+  const onStep1Next = () => {
+    setStep(1)
+  }
+
+  const onStep2Next = async (yourLanguage: Language) => {
+    setYourLanguage(yourLanguage)
+    setLoading(true)
+    setError('')
+    setProbabilities([])
+
+    try {
       const query = gql(`
         query Language($phrase: String!, $modelId: ID!) {
           language(input: { phrase: $phrase, modelId: $modelId}){
@@ -50,59 +99,116 @@ function LanguageClassifierExperiment() {
         setError(result.errors[0].message)
         console.log(JSON.stringify(result.errors))
       } else {
-        const language = result.data.language.mostLikelyLanguage
-        setResult(`Most likely language is ${language}`)
+        setModelLanguage(result.data.language.mostLikelyLanguage)
         setProbabilities(result.data.language.probabilities)
+        setLoading(false)
+        setStep(2)
       }
-      
-      setLoading(false)
     } catch (e: any) {
       console.error(e)
       setError(e.message)
       setLoading(false)
     }
+    
+  }
+
+  const onStep3Next = () => {
+    setPhrase('')
+    setStep(0)
   }
 
   return (
     <>
       <Box id='languageClassifier' displayPrint={'none'} sx={{ m: 2, border: '2px solid white', borderRadius: 2, p: 1 }}>
         <Typography variant="body1">Try it!</Typography>
-        <Typography variant="subtitle2">
-          Enter a phrase in one of the following languages and it will guess what language it is.
-        </Typography>
-        <form>
-          <Grid container spacing={2} p={1}>
-            {/* column 1 */}
-            <Grid item xs={9}>
-              <TextField 
-                variant="filled" 
-                required 
-                fullWidth
-                value={phrase}
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setPhrase(event.target.value)
-                  setResult('')
-                  setProbabilities([])
-                }}
-                onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-                  if (event.key === 'Enter') {
-                    onSubmit()
-                    event.preventDefault()
-                  }
-                }}
-                inputProps={{ placeholder: "Enter your phrase here"}}
-              />
-            </Grid>
-            {/* column 2 */}
-            <Grid item xs={3}>
-              <LoadingButton variant='contained' onClick={onSubmit} loading={isLoading}>Submit</LoadingButton>
-            </Grid>
-          </Grid>
-        </form>
-        { result && <Typography variant='body1'>{result}</Typography>}
-        { probabilities && probabilities.map((probability) =>
-          <Typography key={probability.language}>{probability.language !== Language.Other && `${probability.language}: ${probability.percentageLikely.toFixed(2)}%`}</Typography>
-        )}
+        <Stepper activeStep={step} sx={{ mt: 2 }}>
+          <Step key={1} completed={step > 0}>
+            <StepLabel>Type or load a phrase</StepLabel>
+          </Step>
+          <Step key={2} completed={step > 1}>
+            <StepLabel>Guess the language</StepLabel>
+          </Step>
+          <Step key={3} completed={step > 2}>
+            <StepLabel>Does the model agree?</StepLabel>
+          </Step>
+        </Stepper>
+
+        <Grid container spacing={2} p={2}>
+          { step === 0 &&
+            <>
+              <Grid item xs={12}>
+                <LoadingButton variant='contained' fullWidth onClick={onPickClick} loading={isPicking}>Pick a phrase for me</LoadingButton>
+                <Divider variant="fullWidth" sx={{mt: 3, mb: 1, borderColor: '#888'}}/>
+              </Grid>
+              <Grid item xs={1} mt={1}>
+                <Typography variant='body1'>OR:</Typography>
+              </Grid>
+              <Grid item xs={11}>
+                <TextField
+                  variant="filled"
+                  fullWidth
+                  value={phrase}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setPhrase(event.target.value)
+                  }}
+                  inputProps={{ placeholder: "Enter your phrase here"}}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Stack direction='row'>
+                  <Box sx={{ flex: '1 1 auto' }} />
+                  <Button variant='contained' onClick={onStep1Next} disabled={!phrase}>Next</Button>
+                </Stack>
+              </Grid>
+            </>
+          }
+          { step === 1 &&
+            <>
+              <Grid item xs={12}>
+                <Typography variant='h6'>Phrase:</Typography>
+                <Typography variant='body1'>{phrase}</Typography>
+                <Typography variant='h6'>Take your guess: </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack direction='row' justifyContent='space-between'>
+                  <LoadingButton variant='contained' onClick={ () => onStep2Next(Language.English)} loading={isLoading && yourLanguage === Language.English}>English</LoadingButton>
+                  <LoadingButton variant='contained' onClick={ () => onStep2Next(Language.French)} loading={isLoading && yourLanguage === Language.French}>French</LoadingButton>
+                  <LoadingButton variant='contained' onClick={ () => onStep2Next(Language.German)} loading={isLoading && yourLanguage === Language.German}>German</LoadingButton>
+                  <LoadingButton variant='contained' onClick={ () => onStep2Next(Language.Spanish)} loading={isLoading && yourLanguage === Language.Spanish}>Spanish</LoadingButton>
+                  <LoadingButton variant='contained' onClick={ () => onStep2Next(Language.Swedish)} loading={isLoading && yourLanguage === Language.Swedish}>Swedish</LoadingButton>
+                </Stack>
+              </Grid>
+            </>
+          }
+          { step === 2 &&
+            <>
+              <Grid item xs={12}>
+                <Typography variant='h6'>Phrase:</Typography>
+                <Typography variant='body1'>{phrase}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant='h6'>Your guess:</Typography>
+                <Typography variant='body1'>{yourLanguage}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant='h6'>The model's guess:</Typography>
+                <Typography variant='body1'>{modelLanguage}</Typography>
+                <Typography variant='body1'>Probabilities: {
+                  probabilities && probabilities.map((probability) => 
+                    (probability.language !== Language.Other && `${probability.language}: ${probability.percentageLikely.toFixed(2)}%`) || ''
+                  ).join(' ')
+                }</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Stack direction='row' justifyContent='space-between'>
+                  <Box></Box>
+                  <Button variant='contained' onClick={onStep3Next}>Reset</Button>
+                </Stack>
+              </Grid>
+            </>
+          }
+        </Grid>
+
         { error && <Typography variant='body2' color='#f88'>Error: {error}</Typography>}
         <Divider variant="fullWidth" sx={{mt: 1, mb: 1, borderColor: '#888'}}/>
         <Typography variant="subtitle2">Supported languages: English, French, German, Spanish, Swedish</Typography>
